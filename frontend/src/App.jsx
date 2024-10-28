03047e5fc9c53798b0a8b0fa83c000ba76621e20
Pavan C H <pavandvh27@gmail.com>
2024-10-28 13:45:41 +0530

    navbar added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,18 @@
-import { BrowserRouter as Router, Routes, Route, Link, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import DonarRegistration from "./components/donarRegistration";
 import BloodBankRegistration from "./components/bloodbankRegistration";
+import Login from "./components/login";
+import Nav from "./components/nav";
 
 const App = () => {
     return (
         <Router>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/donar-registration">Donar Registration</Link>
-                    </li>
-                    <li>
-                        <Link to="/bloodbank-registration">Blood Bank Registration</Link>
-                    </li>
-                </ul>
-            </nav>
+            <Nav />
             <Routes>
-                <Route path="/" element={<Navigate to="/" />} />
-                <Route path="/donar-registration" element={<DonarRegistration />} />
-                <Route path="/bloodbank-registration" element={<BloodBankRegistration />} />
+                <Route path="/" element={<Navigate to="/dashboard" />} />
+                <Route path="dashboard/donar-registration" element={<DonarRegistration />} />
+                <Route path="dashboard/bloodbank-registration" element={<BloodBankRegistration />} />
+                <Route path="dashboard/login" element={<Login />} />
             </Routes>
         </Router>
     );
---
+++ frontend/src/components/nav.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+const nav = () => {
+    return (<>
+        <nav>
+            <ul>
+                <li>
+                    <Link to="dashboard/donar-registration">Donar Registration</Link>
+                </li>
+                <li>
+                    <Link to="dashboard/bloodbank-registration">Blood Bank Registration</Link>
+                </li>
+                <li>
+                    <Link to="dashboard/login">Login</Link>
+                </li>
+            </ul>
+        </nav>
+    </>);
+}
+
+export default nav;
